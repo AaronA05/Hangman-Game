3fc93fe66bf82102c8da384d74b2b3cbae5760b6
Aaron Arndt <Aaron Arndt>
2017-08-08 21:03:55 -0500

    Basic Hangman game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 //Have computer pick random word from array - complete
 //Have computer display _ for each letter of word - complete
-//Prompt user to start by typing a letter - complete
-//Check if user guess is a letter of word generated
-//Continue if guess is in word
+//Prompt user to start by typing a letter - complete but using spacebar
+//Check if user guess is a letter of word generated - complete
+//Continue if guess is in word - complete
 //Count down chances if not
 //Chances = 0, new game and restart
 
@@ -13,6 +13,8 @@
 var chances = 5;
 var wins = 0;
 var cpuLetters = [];
+var userPastGuesses = [];
+var letterStorage =[];
 
 //Have computer pick random word from array
 var cpuWords = ["caitlin", "aaron", "domino", "blair"];
@@ -22,49 +24,51 @@ var cpuWordChoice = cpuWords[Math.floor(Math.random() * cpuWords.length)];
 console.log(cpuWordChoice);
 
 
-//Start game with space bar 
-//have computer display _ for each letter of word and push those letters to an array
+//Start game with space bar to show blank letters 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
        for (var j = 0; j < cpuWordChoice.length; j++){
+       	
+       	//have computer store the letters of the word choice for comparison later
+       	letterStorage[j] = cpuWordChoice[j];
+       	console.log(letterStorage);
+       	//have computer display "_" for each letter of word and remove "," from the array
        	cpuLetters[j] = "_";
        	document.getElementById("cpuLetters").textContent = cpuLetters.join(' ');
 		}
 		
-    }
-}
+		//Listen for user start with letters only
+		document.onkeyup = function(event){
+		var userGuess = event.key.toLowerCase();
+		
+			//Checks if user typed in a letter only
+			if (event.keyCode >= 65 && event.keyCode <= 90){
 
+			//Check if user guess is in any letter of cpuWordChoice
+			for (var i = 0; i < cpuWordChoice.length; i++){
+				if (cpuWordChoice[i] === userGuess){
+					cpuLetters[i] = userGuess;
+				}
+			}
 
-
-//Listen for user start on key
-document.onkeyup = function(event){
-	var userGuess = event.key;
-
-//Check if user guess in any letter of cpuWordChoice
-	for (var i = 0; i < cpuWordChoice.length; i++){
-		if (cpuWordChoice[i] === userGuess){
-			cpuLetters[i] = userGuess;
-
-			//This rings up "Correct" for every letter match in the word
-			// console.log("Correct!");
-			// //Pseudo codeo Array.splice;
-			// var correctGuesses = cpuLetters.splice(i, i, userGuess);
-			// console.log(correctGuesses);
-
-			// var cpuLetters = [];
-			// for (var k = 0; k < cpuWordChoice.length; k++){
-			// 	cpuLetters.push(cpuWordChoice.charAt(k));
-			// }
-
-
-			// var fillSpaces = cpuWords.indexof(userGuess);
-			// console.log(fillSpaces);
-			// if (fillSpaces === -1){
-			// 	wordSpaces[fillSpaces] = userGuess;
-			// }
+			//Check if user guess is in previous gueses or is part of game word
+			if (letterStorage.indexOf(userGuess) < 0 && userPastGuesses.indexOf(userGuess) < 0){
+				userPastGuesses.push(userGuess);
+				chances--;
+				console.log("HEY THERE");
+			}
+			
+			
 		}
-	}
 
+
+	document.getElementById("userPastGuesses").textContent = userPastGuesses;
+	document.getElementById("chances").textContent = chances;
 	document.getElementById("cpuLetters").textContent = cpuLetters.join(' ');
 
-}
+		} //on user enter valid letter
+	} //on user enter space bar
+} //on user enter space bar
+
+
+
